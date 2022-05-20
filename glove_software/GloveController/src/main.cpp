9a2a88f34109428e447d47558386def36be6fcb5
Zhivko Y. Krastev <z.y.krastev@protonmail.com>
2022-05-20 22:48:11 +0200

    Auto stash before merge of "main" and "origin/main"

--- glove_software/GloveController/src/main.cpp
+++ glove_software/GloveController/src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const char* SSID = "";
+const char* password = "";
+
+const char* host = "";
+const uint16_t port = 8080;
 
 void setup() 
 {
   // setup sequence
+  Serial.begin(115200);
+  WiFi.begin(SSID, password);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    
+  }
 }
 
 void loop() 
 {
-  
+
 }
