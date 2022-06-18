bc05fc2c6e796daae354dbae05c1f6190bc3f5a4
woid0 <vakes.social@gmail.com>
2022-06-18 10:56:38 +0200

    implemented BendCalibrationLookUpTable

--- src/main/java/at/tugraz/user_interfaces_ss22/mapping/BendCalibrationLookUpTable.java
+++ src/main/java/at/tugraz/user_interfaces_ss22/mapping/BendCalibrationLookUpTable.java
@@ -12,22 +12,43 @@ import java.util.List;
 public class BendCalibrationLookUpTable implements CalibrationLookUpTable {
 
     private final int[] lut = new int[4096];
+    private int maxInput;
+    private int minInput;
 
     @Override
     public void updateTable(@NotNull List<Integer> calibrationInputDataPoints) {
-        // TODO: Update the internal function/LUT with the new calibration data points
-        //  Step1 Find beginning and end of curve (when data goes significantly above zero, and returns back to zero)
-        //  Step2 Use the real data-points (sub-list with start/stop sliced) to calibrate function
-        //  Step3 Store function as an efficient list, so it can be used later every few milliseconds
+
+        this.maxInput = 0;
+        this.minInput = 4095;
+
+        for (Integer currentInput : calibrationInputDataPoints) {
+            this.maxInput = Math.max(currentInput, this.maxInput);
+            this.minInput = Math.min(currentInput, this.minInput);
+        }
+
+        double slope = 4095.0 / (this.maxInput - this.minInput);
+
+        for (int i = 0; i < lut.length; i++){ //TODO: maybe add a buffer-zone here
+            if(i <= this.minInput){
+                lut[i] = 0;
+            }
+            else if(i >= this.maxInput){
+                lut[i] = 4095;
+            }
+            else{
+                lut[i] = (int) (slope * (i - this.minInput));
+            }
+        }
     }
 
     @Override
     public int mapInput(short input) throws IndexOutOfBoundsException {
-        // TODO: retrieve mapping/function and return calibrated value
-        //  Is the function linear? If so, maybe store an array with 4096 values and input==index to look it up?
-        //  if (input < 0) throw new IndexOutOfBoundsException();
-        //  if (input > 4095) throw new IndexOutOfBoundsException();
-        //  if (input > this.max) return 4095;
-        return input;
+
+        if (input < 0) throw new IndexOutOfBoundsException();
+        if (input > 4095) throw new IndexOutOfBoundsException();
+        if (input > this.maxInput) return 4095;
+        if (input < this.minInput) return 0;
+
+        return this.lut[input];
     }
 }
